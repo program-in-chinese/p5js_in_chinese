a1d896db67701fd1a4c64a4ef7787a6425377646
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-09-07 12:20:42 -0700

    将相关属性中文化

--- p5js中文.js
+++ p5js中文.js
@@ -1,3 +1,13 @@
+// 基本属性
+function 帧序号() {
+	return frameCount;
+};
+
+function 宽度() {
+	return width;
+}
+
+// 初始化相关
 function setup() {
 	构图();
 }
@@ -10,14 +20,20 @@ function draw() {
 
 function 绘制() {};
 
+// 二维基本元素
+function 长方形(x位置, y位置, 宽, 高) {
+	rect(x位置, y位置, 宽, 高);
+}
+
+function 三角形(x1, y1, x2, y2, x3, y3) {
+	triangle(x1, y1, x2, y2, x3, y3);
+}
+
 function 椭圆(中心x位置, 中心y位置, 宽, 高) {
 	ellipse(中心x位置, 中心y位置, 宽, 高);
 }
 
-function 新画布(宽, 高) {
-	createCanvas(宽, 高);
-}
-
+// 颜色设置
 function 背景色(灰度) {
 	background(灰度);
 }
@@ -34,17 +50,8 @@ function 笔色(颜色, 透明度) {
 	stroke(颜色, 透明度);
 }
 
-
 function 笔宽(像素数) {
 	strokeWeight(像素数);
-}
-
-function 长方形(x位置, y位置, 宽, 高) {
-	rect(x位置, y位置, 宽, 高);
-}
-
-function 三角形(x1, y1, x2, y2, x3, y3) {
-	triangle(x1, y1, x2, y2, x3, y3);
 }
 
 function 偏移(x, y) {
@@ -90,4 +97,10 @@ function 方形模式(模式) {
 // 模式为RADIANS(默认)或者DEGREES
 function 角度模式(模式) {
   angleMode(模式);
+}
+
+// 其他
+
+function 新画布(宽, 高) {
+	createCanvas(宽, 高);
 }
