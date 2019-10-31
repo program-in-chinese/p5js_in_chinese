42465e744a3930595c9c672d77c84babe256d792
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-10-31 11:49:39 -0700

    提取函数

--- 调皮鬼/调皮鬼.js
+++ 调皮鬼/调皮鬼.js
@@ -30,19 +30,7 @@ function setup() {
   BgColB = random(100, 200);
   frameRate(60)
 }
-
-function draw() {
-  //background(BgColR, BgColG, BgColB);
-  背景色(白色);
-  //grid
-  /*for (let x = 25; x <= width; x += 50) {
-
-      stroke(150);
-      line(x, 0, x, height);
-
-  }*/
-
-  //----Ghost body-----
+function 身体() {
   noStroke();
   fill(body.col);
   beginShape()
@@ -61,23 +49,24 @@ function draw() {
   curveVertex(447, 344);
   curveVertex(390, 318);
   endShape(CLOSE)
+}
 
-  //----Ghost face-----
-
+function 脸() {
   noStroke();
   fill(250);
   ellipse(360, 500, 200, 270)
+}
 
-
-//mouth
+function 嘴() {
   strokeWeight(13);
   stroke(body.col);
   noFill();
   arc(360, 580, 50, 15, 0.2, 3, OPEN);
   strokeWeight(3);
   arc(360, 600, 40, 10, 0.2, 3, OPEN);
+}
 
-  //left upper eyebrow
+function 左上睫毛() {
   noStroke();
   fill(brow.col)
   beginShape()
@@ -89,9 +78,9 @@ function draw() {
   curveVertex(282 + 10, 429);
   curveVertex(280 + 10, 450);
   endShape(CLOSE)
+}
 
-  //left lower eyebrow
-
+function 左下睫毛() {
   noStroke();
   fill(brow.col)
   beginShape()
@@ -103,13 +92,15 @@ function draw() {
   curveVertex(322, 525 + 5);
   curveVertex(318, 498);
   endShape(CLOSE)
+}
 
-  //left eye
+function 左眼() {
   noStroke();
   fill(0);
   ellipse(300, 465, 20 + mouseX / 26, 20)
+}
 
-  //right upper eyebrow
+function 右上睫毛() {
   noStroke();
   fill(brow.col);
   beginShape()
@@ -120,8 +111,9 @@ function draw() {
   curveVertex(392, 434 + 10);
   curveVertex(390, 424 + 10);
   endShape(CLOSE);
+}
 
-  //right lower eyebroW
+function 右下睫毛() {
   noStroke()
   fill(brow.col);
   beginShape()
@@ -132,33 +124,15 @@ function draw() {
   curveVertex(418, 487 + 5);
   curveVertex(418, 466 + 5);
   endShape(CLOSE)
+}
 
-  //right eye - eyes change shapes with mouseX
+function 右眼() {
   noStroke()
   fill(eye.col);
   ellipse(403, 458, 20 + mouseX / 26, 20)
+}
 
-
-
-  //----heart---will glow
-  /*stroke(brow.col);
-  strokeWeight(1 + (frameCount % 17));
-  fill(brow.col)
-  beginShape()
-  curveVertex(159 - 20, 410 - 50);
-  curveVertex(135 - 20, 380 - 50);
-  curveVertex(135 - 20, 365 - 50);
-  curveVertex(142 - 20, 355 - 50);
-  curveVertex(154 - 20, 371 - 50);
-  curveVertex(161 - 20, 350 - 50);
-  curveVertex(173 - 20, 362 - 50);
-  curveVertex(159 - 20, 415 - 50);
-  curveVertex(135 - 20, 380 - 50);
-  endShape(CLOSE)*/
-
-
-
-  //arm
+function 手臂() {
   noStroke()
   fill(body.col);
   var angle = {
@@ -187,9 +161,25 @@ function draw() {
   curveVertex(211, 665);
   curveVertex(285, 625);
   endShape(CLOSE)
+}
 
-  //fingers
+function 手指() {
   noStroke();
   fill(body.col);
   ellipse(170, 560, mouseX / 64, mouseX / 21)
+}
+
+function draw() {
+  背景色(白色);
+  身体()
+  脸()
+  嘴()
+  左上睫毛()
+  左下睫毛()
+  左眼()
+  右上睫毛()
+  右下睫毛()
+  右眼()
+  手臂()
+  手指()
 }
